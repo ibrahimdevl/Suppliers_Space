fd82f60f1d128bfb3fcfa720b3f8eec6f8cf3f45
ibrahimdevl <ibrahimhoussem2000@gmail.com>
2022-08-29 21:08:51 +0100

    authentication is tested

--- src/main/java/com/ibrahimdev/suppliers_space/controllers/TestController.java
+++ src/main/java/com/ibrahimdev/suppliers_space/controllers/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("f")
 public class TestController {
     @GetMapping("/all")
     public String allAccess() {
